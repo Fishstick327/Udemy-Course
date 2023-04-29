be4143f1a5bc98264a09fdda3da25066f3ce8038
Nathan Tran <nathan07tran@gmail.com>
2023-04-29 15:19:09 -0700

    fix the issue relating to directory path

--- snake game/scoreboard.py
+++ snake game/scoreboard.py
@@ -1,12 +1,16 @@
 from turtle import Turtle, Screen
 from food import Food
 import time
+import os
 #make a scoreboard class
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        #the os.path.dirname(__file__) is to get the path of current folder and join it with the file name
+        file_path = os.path.join(os.path.dirname(__file__), "data_score.txt")
         self.score = 0
-        with open("data_score.txt", mode="r") as data_score:
+        #file_path = r"C:\Users\natha\Desktop\WEd Development  bootcamp\Udemy Code\snake game\data_score.txt"
+        with open(file_path, mode="r") as data_score:
             self.high_score = int(data_score.read()) 
             #this read the int value of high score
         self.color("white")
@@ -21,7 +25,9 @@ class Scoreboard(Turtle):
         if self.score > self.high_score:
             self.high_score = self.score    
             #function to write the highest score into a text file and save it
-            with open("data_score.txt", mode ="w") as data_score:
+            
+            file_path = os.path.join(os.path.dirname(__file__), "data_score.txt")
+            with open(file_path, mode ="w") as data_score:
                 print("score saved")
                 data_score.write(f"{self.high_score}") #but when it write it need to be a string
                 print("finished")
